bd7c3d744d4b012ac099c4c8b140571ad347f4d0
krgirard33 <ken.girard@gmail.com>
2017-02-12 20:04:32 -0600

    made base.html, index.html, python imports, class Handler, pointed MainHandler at Handler

--- main.py
+++ main.py
@@ -15,10 +15,33 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+import os
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir),
+    autoescape=True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    
+    def render_str(self, templates, **params):
+        t = jinja_env.get_template(templates)
+        return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainHandler(Handler):
     def get(self):
-        self.response.write('Hello world!')
+        #self.response.write('Hello world!')
+        self.render("index.html")
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
